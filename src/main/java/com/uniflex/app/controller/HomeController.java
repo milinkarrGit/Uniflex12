44805468f3b987cc9f812a13352a9b406a4b2195
milinkarrGit <janviermilinkarr@gmail.com>
2026-04-25 03:32:59 -0400

    fix port railway

--- src/main/java/com/uniflex/app/controller/HomeController.java
+++ src/main/java/com/uniflex/app/controller/HomeController.java
@@ -5,45 +5,23 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Set;
 
-@Controller
+@RestController
 public class HomeController {
 
     @GetMapping("/")
-    public String home(Authentication auth) {
-
-        if (auth == null) {
-            return "redirect:/login";
-        }
-
-        Set<String> roles = AuthorityUtils.authorityListToSet(auth.getAuthorities());
-
-        if (roles.contains("ROLE_ADMIN")) {
-            return "redirect:/admin/dashboard";
-        }
-
-        if (roles.contains("ROLE_TEACHER")) {
-            return "redirect:/teacher/dashboard";
-        }
-
-        if (roles.contains("ROLE_STUDENT")) {
-            return "redirect:/student/dashboard";
-        }
-
-        return "redirect:/login";
+    public String home() {
+        return "App Running";
     }
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
 
-    
     @GetMapping("/")
     @ResponseBody
-    public String test(){
-        return "ok App RUNNING";
+    public String test() {
+        return "ok test";
     }
 }
